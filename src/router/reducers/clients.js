f162b8a0cde6223cb6ac26c960cddb55c286af1f
sheldar@wp.pl <sheldar@wp.pl>
2016-02-09 22:17:52 +0100

    ADD_CLIENT test and implementation

--- src/router/reducers/clients.js
+++ src/router/reducers/clients.js
@@ -5,7 +5,7 @@ const initialState = [];
 module.exports = function clients (state = initialState, action) {
     switch (action.type) {
     case ActionTypes.ADD_CLIENT:
-        return state;
+        return [action.client, ...state];
     default:
         return state;
     }
--- test/reducers/clients.spec.js
+++ test/reducers/clients.spec.js
@@ -1,10 +1,19 @@
-const expect = require('expect');
-const clients = require('../../src/router/reducers/clients');
 const ActionTypes = require('../../src/router/constants/ActionTypes');
+const clients = require('../../src/router/reducers/clients');
+const expect = require('expect');
+const net = require('net');
 
 describe('clients reducer', () => {
     it('should handle initial state', () => {
-        expect(clients(undefined, {}))
-            .toEqual([]);
+        expect(clients(undefined, {})).toEqual([]);
+    });
+
+    it('should handle ADD_CLIENT', () => {
+        var client = new net.Socket();
+
+        expect(clients([], {
+            client: client,
+            type: ActionTypes.ADD_CLIENT
+        })).toEqual([client]);
     });
 });
